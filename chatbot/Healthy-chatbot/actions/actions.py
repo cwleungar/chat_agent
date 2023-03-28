4517e0ade5626605297e28b49a9510cb6c9824b6
mynameisissac <kaikaijjj123@gmail.com>
2023-03-28 19:46:24 +0800

    Add: form asking user's personal info

--- chatbot/Healthy-chatbot/actions/actions.py
+++ chatbot/Healthy-chatbot/actions/actions.py
@@ -7,47 +7,52 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
 
-
 #### custom action Start here:
 
 
 # action_receive_name
 # the chatbot will set the slot "name" to the text it received from user
-class ActionReceiveFirstName(Action):
+class ActionReceiveName(Action):
 
     def name(self) -> Text:
-        return "action_receive_firstname"
+        return "action_receive_name"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        
-        dispatcher.utter_message(text="Pleas give me your first name")        # say something to user
         text_received = tracker.latest_message['text']          # use tracker to get the message history from user
-
-        # return a list of event
-        return [SlotSet("first_name", text_received)]                 # set the slot first_name
-
-
-class ActionReceiveLastName(Action):
-
-    def name(self) -> Text:
-        return "action_receive_lastname"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+        dispatcher.utter_message(text=f"{text_received}, hi nice to meet you")        # say something to user
         
-        dispatcher.utter_message(text="Pleas give me your last name")        # say something to user
-        text_received = tracker.latest_message['text']                      # use tracker to get the message history from user
-
-
         # return a list of event
-        return [SlotSet("last_name", text_received)]                 # set the slot last_name
+        return [SlotSet("name", text_received)]                 # set the slot first_name
 
 
+# calculate BMI based on weight and height slots
+class ActionCalculateBmi(Action):
+    
+        def name(self) -> Text:
+            return "action_calculate_bmi"
+    
+        def run(self, dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    
+            weight = tracker.get_slot("weight")         # get the value in slot "weight"
+            height = tracker.get_slot("height")         # get the value in slot "height"
+            
+            # if weight or height is None, i.e. havent get user's weight or height
+            if not weight or not height:
+                dispatcher.utter_message(text="I dont have enough information for you BMI now. Sorry.")
+            else:
+                bmi = weight / (height/100)**2
+                dispatcher.utter_message(text=f"Your BMI is {bmi}!")        
+                
+            # return a list of event
+            return [SlotSet("bmi", bmi)]
+
+
+## Users asking the bot whether it know his/her some information
 # action_say_name
 # the chatbot will say the name of user when user ask it to do so
 class ActionSayName(Action):
@@ -69,3 +74,65 @@ class ActionSayName(Action):
         
         # return a list of event
         return []
+
+# the chatbot will say the name of user when user ask it to do so
+class ActionSayAge(Action):
+
+    def name(self) -> Text:
+        return "action_say_age"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        age = tracker.get_slot("age")         # get the value in slot "age"
+        
+        # if age is None, i.e. havent get user's age
+        if not age:
+            dispatcher.utter_message(text="I dont have your age wor ")
+        else:            
+            dispatcher.utter_message(text=f"Your age is {age}!")        
+        
+        # return a list of event
+        return []
+
+class ActionSayWeight(Action):
+
+    def name(self) -> Text:
+        return "action_say_weight"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        weight = tracker.get_slot("weight")         # get the value in slot "weight"
+        
+        # if weight is None, i.e. havent get user's weight
+        if not weight:
+            dispatcher.utter_message(text="I dont have your weight wor ")
+        else:            
+            dispatcher.utter_message(text=f"Your weight is {weight}!")        
+        
+        # return a list of event
+        return []
+
+class ActionSayBmi(Action):
+
+    def name(self) -> Text:
+        return "action_say_bmi"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        bmi = tracker.get_slot("bmi")         # get the value in slot "bmi"
+        
+        # if bmi is None, i.e. havent get user's bmi
+        if not bmi:
+            dispatcher.utter_message(text="I dont have enough information for you BMI now. Sorry.")
+        else:            
+            dispatcher.utter_message(text=f"Your BMI is {bmi}!")        
+        
+        # return a list of event
+        return []
+
